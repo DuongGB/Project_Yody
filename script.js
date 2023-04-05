477594b760c3846e2b80a5dbd08789ceb9ca6e23
Duong-GB <duongnguyenqn1323@gmail.com>
2023-04-05 21:19:07 +0700

    finished 60%

---
+++ script.js
@@ -0,0 +1,12 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+const menuItem = $$(".menu-item");
+const female = $(".menu-sidebar-wrapper__female");
+
+// menuItem.forEach((element) => {
+//   element[0].onclick = () => {
+//     female.classList.remove("d-none");
+//     this.classList.add("select");
+//   };
+// });
